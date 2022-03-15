7ec3682f1ebac8eefa4fa4c02e0c8d3f48d7be76
UmidaImerova <umidagaliamova@gmail.com>
2022-03-15 12:25:58 +0600

    data created as array

--- src/data/data.js
+++ src/data/data.js
@@ -1,135 +1,135 @@
+export const menu= [{
+    id: 1,
+    category:"breakfast",
+    title: "Fried eggs with branded sausages",
+    discription: "Branded chicken sausages, fried eggs - fried eggs, crispy toast.",
+    weight: "200",
+    img: 'https://mobile.mypizza.kg/jpg/%D0%AF%D0%B8%D1%87%D0%BD%D0%B8%D1%86%D0%B0%20%D1%81%20%D1%84%D0%B8%D1%80%D0%BC%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8%20%D0%BA%D0%BE%D0%BB%D0%B1%D0%B0%D1%81%D0%BA%D0%B0%D0%BC%D0%B8.jpg',
+    price: 150
+} ,
 {
-    id: 1;
-    category:"breakfast";
-    title: "Fried eggs with branded sausages";
-    discription: "Branded chicken sausages, fried eggs - fried eggs, crispy toast.";
-    weight: "200";
-    img: 'https://mobile.mypizza.kg/jpg/%D0%AF%D0%B8%D1%87%D0%BD%D0%B8%D1%86%D0%B0%20%D1%81%20%D1%84%D0%B8%D1%80%D0%BC%D0%B5%D0%BD%D0%BD%D1%8B%D0%BC%D0%B8%20%D0%BA%D0%BE%D0%BB%D0%B1%D0%B0%D1%81%D0%BA%D0%B0%D0%BC%D0%B8.jpg';
-    price: "150"
-} 
+    id: 2,
+    title: "Granola",
+    category:"breakfast",
+    discription: "Granola, greek yogurt, fresh fruits and berries.",
+    weight: "***",
+    img: 'https://mobile.mypizza.kg/jpg/%D0%B3%D1%80%D0%B0%D0%BD%D0%BE%D0%BB%D0%B0.jpg',
+    price: 110
+},
 {
-    id: 2;
-    title: "Granola";
-    category:"breakfast";
-    discription: "Granola, greek yogurt, fresh fruits and berries.";
-    weight: "***";
-    img: 'https://mobile.mypizza.kg/jpg/%D0%B3%D1%80%D0%B0%D0%BD%D0%BE%D0%BB%D0%B0.jpg';
-    price: "110"
-}
+    id: 3,
+    title: "Pancakes with cottage cheese",
+    category:"breakfast",
+    discription: "Pancakes with cottage cheese.",
+    weight: '3 p.',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%91%D0%BB%D0%B8%D0%BD%D1%8B%20%D1%81%20%D1%82%D0%B2%D0%BE%D1%80%D0%BE%D0%B3%D0%BE%D0%BC.jpg',
+    price: 100
+},
 {
-    id: 3;
-    title: "Pancakes with cottage cheese";
-    category:"breakfast";
-    discription: "Pancakes with cottage cheese.";
-    weight: 3
-    img: 'https://mobile.mypizza.kg/jpg/%D0%91%D0%BB%D0%B8%D0%BD%D1%8B%20%D1%81%20%D1%82%D0%B2%D0%BE%D1%80%D0%BE%D0%B3%D0%BE%D0%BC.jpg';
-    price: "110"
-}
+    id: 4,
+    title: "Coirasan",
+    category:"breakfast",
+    discription: "With mild slated salmon, Philadelphia cheese, cucumber, Iceberg lettuce and herbs.",
+    weight: '***',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%80%D1%83%D0%B0%D1%81%D1%81%D0%B0%D0%BD%20%D1%84%D0%B8%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D0%B8%D1%8F.png',
+    price: 200
+},
 {
-    id: 4;
-    title: "Coirasan";
-    category:"breakfast";
-    discription: "With mild slated salmon, Philadelphia cheese, cucumber, Iceberg lettuce and herbs.";
-    weight: 1;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%80%D1%83%D0%B0%D1%81%D1%81%D0%B0%D0%BD%20%D1%84%D0%B8%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D0%B8%D1%8F.png';
-    price: "200"
-}
+    id: 5,
+    title: "Porridge",
+    category:"breakfast",
+    discription: "Porridge on natural milk from rice, with the addition of butter.",
+    weight: '320',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%80%D1%83%D0%B0%D1%81%D1%81%D0%B0%D0%BD%20%D1%84%D0%B8%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D0%B8%D1%8F.png',
+    price: 88
+},
 {
-    id: 5;
-    title: "Porridge";
-    category:"breakfast";
-    discription: "Porridge on natural milk from rice, with the addition of butter.";
-    weight: 320;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%80%D1%83%D0%B0%D1%81%D1%81%D0%B0%D0%BD%20%D1%84%D0%B8%D0%BB%D0%B0%D0%B4%D0%B5%D0%BB%D1%8C%D1%84%D0%B8%D1%8F.png';
-    price: "88"
-}
+    id: 6,
+    title: "Chicken Pizza.",
+    category:"pizza",
+    discription: "Pizza sauce, mozzarella, chicken breast in cream sauce, greens. on natural milk from rice, with the addition of butter.",
+    weight: '710',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%83%D1%80%D0%B8%D0%BD%D0%B0%D1%8F.jpg',
+    price: 498
+},
 {
-    id: 6;
-    title: "Chicken Pizza.";
-    category:"pizza";
-    discription: "Pizza sauce, mozzarella, chicken breast in cream sauce, greens. on natural milk from rice, with the addition of butter.";
-    weight: 710;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%83%D1%80%D0%B8%D0%BD%D0%B0%D1%8F.jpg';
-    price: "498"
-}
+    id: 7,
+    title: "Pepperoni Pizza",
+    category:"pizza",
+    discription: "Pizza sauce, Mozzarella, salami, champignon mushrooms.",
+    weight: '510',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%83%D1%80%D0%B8%D0%BD%D0%B0%D1%8F.jpg',
+    price: 498
+},
 {
-    id: 7;
-    title: "Pepperoni Pizza";
-    category:"pizza";
-    discription: "Pizza sauce, Mozzarella, salami, champignon mushrooms.";
-    weight: 510;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%83%D1%80%D0%B8%D0%BD%D0%B0%D1%8F.jpg';
-    price: "398"
-}
+    id: 8,
+    title: "Thai pizza",
+    category:"pizza",
+    discription: "Pad Thai sauce, Mozzarella, marinated chicken, oyster mushrooms, pickled onions, jalapeno peppers, cilantro.",
+    weight: '620',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%83%D1%80%D0%B8%D0%BD%D0%B0%D1%8F.jpg',
+    price: 478
+},
 {
-    id: 8;
-    title: "Thai pizza";
-    category:"pizza";
-    discription: "Pad Thai sauce, Mozzarella, marinated chicken, oyster mushrooms, pickled onions, jalapeno peppers, cilantro.";
-    weight: 620;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%9A%D1%83%D1%80%D0%B8%D0%BD%D0%B0%D1%8F.jpg';
-    price: "478"
-}
+    id: 9,
+    title: "Warm salad with chicken liver and mushrooms.",
+    category:"salad",
+    discription: "Fried chicken liver with oyster mushrooms and onions, mix of lettuce leaves, tomatoes, roasted sunflower seeds. Served with raspberry sauce.",
+    weight: '240',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%A1%D0%B0%D0%BB%D0%B0%D1%82%20%D0%A2%D1%91%D0%BF%D0%BB%D1%8B%D0%B9%20%D1%81%D0%B0%D0%BB%D0%B0%D1%82%20%D1%81%20%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D0%BE%D0%B9%20%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%D1%8E.jpg',
+    price: 270
+},
 {
-    id: 9;
-    title: "Warm salad with chicken liver and mushrooms.";
-    category:"salad";
-    discription: "Fried chicken liver with oyster mushrooms and onions, mix of lettuce leaves, tomatoes, roasted sunflower seeds. Served with raspberry sauce.";
-    weight: 240;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%A1%D0%B0%D0%BB%D0%B0%D1%82%20%D0%A2%D1%91%D0%BF%D0%BB%D1%8B%D0%B9%20%D1%81%D0%B0%D0%BB%D0%B0%D1%82%20%D1%81%20%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D0%BE%D0%B9%20%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%D1%8E.jpg';
-    price: "270"
-}
+    id: 10,
+    title: "Warm salad with chicken liver and mushrooms.",
+    category:"salad",
+    discription: "Fried chicken liver with oyster mushrooms and onions, mix of lettuce leaves, tomatoes, roasted sunflower seeds. Served with raspberry sauce.",
+    weight: '240',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%A1%D0%B0%D0%BB%D0%B0%D1%82%20%D0%A2%D1%91%D0%BF%D0%BB%D1%8B%D0%B9%20%D1%81%D0%B0%D0%BB%D0%B0%D1%82%20%D1%81%20%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D0%BE%D0%B9%20%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%D1%8E.jpg',
+    price: 270
+},
 {
-    id: 10;
-    title: "Warm salad with chicken liver and mushrooms.";
-    category:"salad";
-    discription: "Fried chicken liver with oyster mushrooms and onions, mix of lettuce leaves, tomatoes, roasted sunflower seeds. Served with raspberry sauce.";
-    weight: 240;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%A1%D0%B0%D0%BB%D0%B0%D1%82%20%D0%A2%D1%91%D0%BF%D0%BB%D1%8B%D0%B9%20%D1%81%D0%B0%D0%BB%D0%B0%D1%82%20%D1%81%20%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D0%BE%D0%B9%20%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%D1%8E.jpg';
-    price: "270"
-}
+    id: 11,
+    title: "Warm rols",
+    category:"susi/rol",
+    discription: "Fried chicken liver with oyster mushrooms and onions, mix of lettuce leaves, tomatoes, roasted sunflower seeds. Served with raspberry sauce.",
+    weight: '240',
+    img: 'https://mobile.mypizza.kg/jpg/%D0%A1%D0%B0%D0%BB%D0%B0%D1%82%20%D0%A2%D1%91%D0%BF%D0%BB%D1%8B%D0%B9%20%D1%81%D0%B0%D0%BB%D0%B0%D1%82%20%D1%81%20%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D0%BE%D0%B9%20%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%D1%8E.jpg',
+    price: 270
+},
 {
-    id: 11;
-    title: "Warm rols";
-    category:"susi/rol";
-    discription: "Fried chicken liver with oyster mushrooms and onions, mix of lettuce leaves, tomatoes, roasted sunflower seeds. Served with raspberry sauce.";
-    weight: 240;
-    img: 'https://mobile.mypizza.kg/jpg/%D0%A1%D0%B0%D0%BB%D0%B0%D1%82%20%D0%A2%D1%91%D0%BF%D0%BB%D1%8B%D0%B9%20%D1%81%D0%B0%D0%BB%D0%B0%D1%82%20%D1%81%20%D0%BA%D1%83%D1%80%D0%B8%D0%BD%D0%BE%D0%B9%20%D0%BF%D0%B5%D1%87%D0%B5%D0%BD%D1%8C%D1%8E.jpg';
-    price: "270"
-}
+    id: 12,
+    title: "Chuchvara",
+    category:"soup",
+    discription: "A dish of Central Asian cuisine in the form of a rich aromatic broth with boiled lamb dumplings.",
+    weight: '****',
+    img: "https://mobile.mypizza.kg/jpg/%D0%A7%D1%83%D1%87%D0%B2%D0%B0%D1%80%D0%B0.jpg",
+    price: 290
+},
 {
-    id: 12;
-    title: "Chuchvara";
-    category:"soup";
-    discription: "A dish of Central Asian cuisine in the form of a rich aromatic broth with boiled lamb dumplings.";
-    weight: '****';
-    img: "https://mobile.mypizza.kg/jpg/%D0%A7%D1%83%D1%87%D0%B2%D0%B0%D1%80%D0%B0.jpg";
-    price: "290"
-}
+    id: 13,
+    title: "Meatballs with sour cream.",
+    category:"lunch",
+    discription: "Meatballs cooked in beef broth with passaged vegetables.",
+    weight: '350',
+    img: "https://mobile.mypizza.kg/jpg/%D0%9C%D1%8F%D1%81%D0%BD%D1%8B%D0%B5%20%D1%88%D0%B0%D1%80%D0%B8%D0%BA%D0%B8.jpg",
+    price: 310
+},
 {
-    id: 13;
-    title: "Meatballs with sour cream.";
-    category:"lunch";
-    discription: "Meatballs cooked in beef broth with passaged vegetables.";
-    weight: 350
-    img: "https://mobile.mypizza.kg/jpg/%D0%9C%D1%8F%D1%81%D0%BD%D1%8B%D0%B5%20%D1%88%D0%B0%D1%80%D0%B8%D0%BA%D0%B8.jpg";
-    price: "310"
-}
+    id: 14,
+    title: "Cheasecake",
+    category:"desert",
+    discription: "A soft curd dessert based on Philadelphia cheese with a fresh creamy aroma and a delicate sour taste",
+    weight: '***',
+    img: "https://mobile.mypizza.kg/jpg/2021/%D0%94%D0%B5%D1%81%D0%B5%D1%80%D1%82%D1%8B/%D0%A7%D0%B8%D0%B7%D0%BA%D0%B5%D0%B9%D0%BA_%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9_2021.jpg",
+    price: 200
+},
 {
-    id: 14;
-    title: "Cheasecake";
-    category:"desert";
-    discription: "A soft curd dessert based on Philadelphia cheese with a fresh creamy aroma and a delicate sour taste";
-    weight: '***'
-    img: "https://mobile.mypizza.kg/jpg/2021/%D0%94%D0%B5%D1%81%D0%B5%D1%80%D1%82%D1%8B/%D0%A7%D0%B8%D0%B7%D0%BA%D0%B5%D0%B9%D0%BA_%D0%BD%D0%BE%D0%B2%D1%8B%D0%B9_2021.jpg";
-    price: "200"
-}
-{
-    id: 15;
-    title: "Juce";
-    category:"drink";
-    discription: "J7";
-    weight: '***'
-    img: "https://mobile.mypizza.kg/jpg/2021/19.jpg";
-    price: "198"
-}
+    id: 15,
+    title: "Juce",
+    category:"drink",
+    discription: "J7",
+    weight: '***',
+    img: "https://mobile.mypizza.kg/jpg/2021/19.jpg",
+    price: 198
+}]
